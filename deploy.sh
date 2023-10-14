1daefe56fa9a775eda49afc3512b4f0399043c06
martin ogg <2martinogg@gmail.com>
2023-10-14 14:15:04 +0900

    fix cut for awk for termux (termux cut is different to terminal)

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-final=$(ifconfig | grep '192' | cut -d ' ' -f 2) 
+final=$(ifconfig | grep '192.' | awk '{print $2}') 
 timestamp=$(date +"%Y-%m-%d--%H:%M:%S")
 cp ./dispTemplate.txt ./index.htm
 sed -i .del "s/__IP__/"$final"/g" ./index.htm
