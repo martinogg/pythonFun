7330554caef28fff44dc6a891411341b4791c458
martin ogg <2martinogg@gmail.com>
2023-10-14 14:38:44 +0900

    mac deploy script (original one has slight syntax differences, for termux)
    template update

--- deployMac.sh
+++ deployMac.sh
@@ -1,10 +1,6 @@
 final=$(ifconfig | grep '192.' | awk '{print $2}') 
 timestamp=$(date +"%Y-%m-%d--%H:%M:%S")
-batteryper=%(termux-battery-status | grep 'percentage')
-
 cp ./dispTemplate.txt ./index.htm
-sed -i "s/__IP__/"$final"/g" ./index.htm
-sed -i "s/__TIME__/"$timestamp"/g" ./index.htm
-sed -i "s/__BATTERY__/"$batteryper"/g" ./index.htm
-
+sed -i .del "s/__IP__/"$final"/g" ./index.htm
+sed -i .del "s/__TIME__/"$timestamp"/g" ./index.htm
 lftp martinogg.com -u serverstatus_martinogg.com:martin -p 21 -e 'put ./index.htm; bye'
